b13548fba9759b27d3423f7f9d0a5113fe6fedfb
Brandon Kelton <brandon.kelton@outlook.com>
2019-04-14 08:31:07 -0600

    Add project files.

---
+++ InteractiveServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InteractiveServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
